0568ac35418b14f3f818a53afbfe61df966a0079
Patiphon Loetsuthakun <36232545+Phon1209@users.noreply.github.com>
2023-07-12 15:42:45 -0400

    Made changable dropdown

--- app/components/navigation/CatalogDropdown.jsx
+++ app/components/navigation/CatalogDropdown.jsx
@@ -1,37 +1,69 @@
 "use client";
 
 import React, { useState } from "react";
-import Image from "next/image";
-import ChevronUp from "/public/assets/svg/chevron-up.svg";
-import ChevronDown from "/public/assets/svg/chevron-down.svg";
+import ChevronUp from "@/public/assets/svg/chevron-up.svg";
+import ChevronDown from "@/public/assets/svg/chevron-down.svg";
+import { useAppContext } from "@/app/contexts/appContext/AppProvider";
+
+const catalogList = [
+  {
+    text: "2022 - 2023 Catalog",
+    value: 2023,
+  },
+  {
+    text: "2021 - 2022 Catalog",
+    value: 2022,
+  },
+  {
+    text: "2020 - 2021 Catalog",
+    value: 2021,
+  },
+  {
+    text: "2019 - 2020 Catalog",
+    value: 2020,
+  },
+  {
+    text: "2018 - 2019 Catalog",
+    value: 2019,
+  },
+];
 
 const CatalogDropdown = () => {
+  const { catalog_year, setCatalog } = useAppContext();
   const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const catalogText = catalogList.filter((cat) => cat.value === catalog_year)[0]
+    .text;
 
   return (
     <div
-      className="dropdown flex items-center gap-2 w-[320px] bg-white border-solid border border-gray-300 text-gray-500 py-[10px] px-[14px] rounded-lg"
+      className="dropdown flex items-center gap-2 w-[320px] bg-white border-solid border border-gray-300 text-gray-500 py-[10px] px-[14px] rounded-lg prevent-select"
       onClick={() => {
         setDropdownOpen((open) => !open);
       }}
     >
-      <div className="grow">2022 - 2023 Catalog</div>
+      <div className="grow">{catalogText}</div>
       <div>
-        <Image
-          src={dropdownOpen ? ChevronUp.src : ChevronDown.src}
-          alt="dropdown icon"
-          width={20}
-          height={20}
-        ></Image>
+        {!dropdownOpen && <ChevronDown />}
+        {dropdownOpen && <ChevronUp />}
       </div>
 
       {dropdownOpen && (
         <div className="dropdown-choices ut-shadow-lg">
-          <div className="dropdown-choice">2022 - 2023 Catalog</div>
-          <div className="dropdown-choice">2021 - 2022 Catalog</div>
-          <div className="dropdown-choice">2020 - 2021 Catalog</div>
-          <div className="dropdown-choice">2019 - 2020 Catalog</div>
-          <div className="dropdown-choice">2018 - 2019 Catalog</div>
+          {catalogList.map((choice) => {
+            return (
+              <div
+                className="dropdown-choice"
+                key={choice.value}
+                value={choice.value}
+                onClick={() => {
+                  setCatalog(choice.value);
+                }}
+              >
+                {choice.text}
+              </div>
+            );
+          })}
         </div>
       )}
     </div>
