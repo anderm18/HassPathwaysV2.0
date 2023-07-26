cc62f55673b5a4ca15171cecd8c0f74bceeae101
powe97 <116031952+powe97@users.noreply.github.com>
2023-07-26 15:08:23 -0400

    coursesearch API

---
+++ app/api/courseSearch/route.ts
@@ -0,0 +1,58 @@
+import { NextResponse } from 'next/server';
+import cors from "cors";
+
+const tags_short_to_long = {
+    "HInq": "HASS Inquiry",
+    "CI": "Communication Intensive",
+    "PDII": "PDII Option for Engr Majors"
+}
+const hass_prefixes = [
+    "ARTS",
+    "COGS",
+    "COMM",
+    "ECON",
+    "GSAS",
+    "INQR",
+    "ITWS",
+    "LANG",
+    "LITR",
+    "PHIL",
+    "PSYC",
+    "STSO",
+    "WRIT"
+]
+
+export async function GET(request: NextRequest) {
+    const params = request.nextUrl.searchParams;
+    
+    const hass_courses_desc = Object.fromEntries(Object.entries(
+        await(
+            await fetch("https://raw.githubusercontent.com/quatalog/data/master/catalog.json")
+        )
+        .json())
+        .filter(c => hass_prefixes.includes(c[0].substring(0,4))));
+    const hass_courses_attributes = Object.entries(
+        await(
+            await fetch("https://raw.githubusercontent.com/quatalog/data/master/prerequisites.json")
+        )
+        .json())
+        .filter(c => Object.keys(hass_courses_desc).includes(c[0]));
+
+    var blob = hass_courses_attributes;
+    if(params.get("prefix")) {
+        blob = blob.filter(c => params.get("prefix").includes(c[0].substring(0,4)));
+    }
+    if(params.get("level")) {
+        blob = blob.filter(c => params.get("level").includes(c[0].substring(5,6)));
+    }
+    if(params.get("filter")) {
+        blob = blob.filter(c => params.get("filter").slice(1,-1).split(",").map(p => tags_short_to_long[p]).every(v => c[1]["attributes"].includes(v)));
+    }
+    var res = [];
+    blob.forEach(c => res.push({ title : hass_courses_desc[c[0]]["name"], courseCode : c[0], tag : c[1]["attributes"] }));
+    if(params.get("searchString")) {
+        res = res.filter(c => c["title"].toLowerCase().includes(params.get("searchString").toLowerCase()));
+    }
+    
+    return NextResponse.json({ res });
+}
