f68b4ef364fda67f2c7e4efcefb7d3e1a0c32cec
Patiphon Loetsuthakun <36232545+Phon1209@users.noreply.github.com>
2023-08-03 00:55:35 -0400

    hard-coded pathway category

---
+++ public/data/pathwaysCategories.ts
@@ -0,0 +1,12 @@
+const pathwaysCategories = {
+  ART: { display: "Art", value: "Arts" },
+  COGSCI: { display: "CogSci", value: "Cognitive Science" },
+  COMM: { display: "Comm", value: "Communication & Media" },
+  ECON: { display: "Econ", value: "Economics" },
+  INTER: { display: "Inter", value: "Interdisciplinary" },
+  // LANG: {display: "Lang", value: ""}
+  // MAJOR: { display: "Major Restricted", value: "Major Restricted" },
+  STS: { display: "STS", value: "STS" },
+};
+
+export default pathwaysCategories;
