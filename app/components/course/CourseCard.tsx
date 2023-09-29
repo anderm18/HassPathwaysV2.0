be9744b76b37a0982fbd24d5c48f6b8c4ea9563c
Jiawei-Zhao-728 <zhaoranjiawei@gmail.com>
2023-09-29 16:11:38 -0400

    added clikable title

--- app/components/course/CourseCard.tsx
+++ app/components/course/CourseCard.tsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { CourseCardProps } from "@/app/model/CourseInterface";
 
 const CourseCard = ({ title, courseCode, tag }: CourseCardProps) => {
+  const [showInfo, setShowInfo] = useState(false);
+  const toggleInfo = () => {
+    setShowInfo(!showInfo);
+  };
+
   return (
     <section className="course-card">
       <div className="flex flex-col fold:flex-row justify-between items-start">
         <header className="course-title flex-shrink-1">
-          <h3 className="text-md font-semibold break-normal">{title}</h3>
+          <button
+            className="text-md font-semibold break-normal"
+            onClick={toggleInfo}
+          >
+            {title}
+          </button>
           <p className="text-sm text-gray-600">{courseCode}</p>
         </header>
         <div className="text-sm font-semibold text-gray-25 bg-primary-700 px-4 py-2.5 border border-solid border-gray-300 rounded-lg">
