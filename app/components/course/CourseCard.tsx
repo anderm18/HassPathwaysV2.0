5040de170f722808e9d7a1fd9a86c10bde6b3135
munipa <akhilmunipalli236@gmail.com>
2023-10-10 16:20:24 -0400

    Added the chevron-down button to the Course Card component, so that a drop down menu can be implemented when a user hovers and clicks on the chevron down button.

--- app/components/course/CourseCard.tsx
+++ app/components/course/CourseCard.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { CourseCardProps } from "@/app/model/CourseInterface";
+import ChevronDown from "@/public/assets/svg/chevron-down-white.svg?svgr";
 
 const CourseCard = ({ title, courseCode, tag }: CourseCardProps) => {
+  
   return (
     <section className="course-card">
       <div className="flex flex-col fold:flex-row justify-between items-start">
@@ -9,8 +11,11 @@ const CourseCard = ({ title, courseCode, tag }: CourseCardProps) => {
           <h3 className="text-md font-semibold break-normal">{title}</h3>
           <p className="text-sm text-gray-600">{courseCode}</p>
         </header>
-        <div className="text-sm font-semibold text-gray-25 bg-primary-700 px-4 py-2.5 border border-solid border-gray-300 rounded-lg">
-          Placeholder
+        <div className="w-[135px] h-10 px-4 py-2.5 bg-primary-700 rounded-lg shadow border border-gray-300 flex items-center justify-between gap-2">
+          <div className="text-sm font-semibold text-gray-25">Select</div>
+          <div className="w-5 h-5 relative">
+            {<ChevronDown />}
+          </div>
         </div>
       </div>
       <div className="flex gap-x-1 flex-wrap flex-grow-0 flex-shrink-0">
