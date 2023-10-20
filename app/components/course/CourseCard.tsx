916844052b8207a95c3a9a0405401c4432c9ec84
munipa <akhilmunipalli236@gmail.com>
2023-10-20 16:07:45 -0400

    inserted a blank dropdown menu into coursecard component

--- app/components/course/CourseCard.tsx
+++ app/components/course/CourseCard.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { CourseCardProps } from "@/app/model/CourseInterface";
 import ChevronDown from "@/public/assets/svg/chevron-down-white.svg?svgr";
+import CourseDropdown from "./CourseDropdown";
 
 const CourseCard = ({ title, courseCode, tag }: CourseCardProps) => {
-  
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
+
   return (
     <section className="course-card">
       <div className="flex flex-col fold:flex-row justify-between items-start">
@@ -11,10 +17,11 @@ const CourseCard = ({ title, courseCode, tag }: CourseCardProps) => {
           <h3 className="text-md font-semibold break-normal">{title}</h3>
           <p className="text-sm text-gray-600">{courseCode}</p>
         </header>
-        <div className="w-[135px] h-10 px-4 py-2.5 bg-primary-700 rounded-lg shadow border border-gray-300 flex items-center justify-between gap-2">
-          <div className="text-sm font-semibold text-gray-25">Select</div>
-          <div className="w-5 h-5 relative">
-            {<ChevronDown />}
+        <div className="w-[135px] h-10 px-4 py-2.5 bg-primary-700 rounded-lg shadow border border-gray-300 flex items-center justify-center gap-2">
+          <div className="text-sm font-semibold text-gray-25">Placeholder</div>
+          <div className="w-5 h-5 relative" onClick={toggleDropdown}>
+            <ChevronDown />
+            {isDropdownOpen && <CourseDropdown />}
           </div>
         </div>
       </div>
