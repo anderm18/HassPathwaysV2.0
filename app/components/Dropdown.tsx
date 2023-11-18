c05ad53824b61f3f04db0b4e421e6be72ae485f1
munipa <akhilmunipalli236@gmail.com>
2023-11-18 14:44:58 -0500

    Updated Dropdown functionality where user can now select different dropdown items

--- app/components/Dropdown.tsx
+++ app/components/Dropdown.tsx
@@ -9,7 +9,13 @@ function classNames(...classes) {
 }
 
 export default function Example() {
+  const [selectedItem, setSelectedItem] = useState("Not Selected");
   const [open, setOpen] = useState(false);
+
+  const handleSelect = (item) => {
+    setSelectedItem(item);
+    setOpen(false);
+  };
 
   return (
     <Menu as="div" className="relative inline-block text-left">
@@ -18,13 +24,13 @@ export default function Example() {
           className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
           onClick={() => setOpen(!open)}
         >
-          Not Selected
+          {selectedItem}
           {open ? <ChevronUp className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" /> : <ChevronDown className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />}
         </Menu.Button>
       </div>
 
       <Transition
-        as={Fragment}
+        as= "div"
         enter="transition ease-out duration-100"
         enterFrom="transform opacity-0 scale-95"
         enterTo="transform opacity-100 scale-100"
@@ -33,7 +39,15 @@ export default function Example() {
         leaveTo="transform opacity-0 scale-95"
       >
         {open && (
-          <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <Menu.Items 
+          className={classNames(
+            "absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none",
+            { "bg-red-200": selectedItem === "Completed" }, // Change bg-red-200 to the desired style
+            { "bg-green-200": selectedItem === "In Progress" }, // Change bg-green-200 to the desired style
+            { "bg-blue-200": selectedItem === "Planned" }, // Change bg-blue-200 to the desired style
+            { "bg-yellow-200": selectedItem === "Not Selected" } // Change bg-yellow-200 to the desired style
+          )}
+        >
             <div className="py-1">
               <Menu.Item>
                 {({ active }) => (
@@ -43,6 +57,7 @@ export default function Example() {
                       active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
                       'block px-4 py-2 text-sm'
                     )}
+                    onClick={() => handleSelect("Completed")}
                   >
                     Completed
                   </a>
@@ -51,11 +66,12 @@ export default function Example() {
               <Menu.Item>
                 {({ active }) => (
                   <a
-                    href="#"
+                    href="#" 
                     className={classNames(
-                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                      'block px-4 py-2 text-sm'
+                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                        'block px-4 py-2 text-sm'
                     )}
+                    onClick={() => handleSelect("In Progress")}
                   >
                     In Progress
                   </a>
@@ -69,6 +85,7 @@ export default function Example() {
                       active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
                       'block px-4 py-2 text-sm'
                     )}
+                    onClick={() => handleSelect("Planned")}
                   >
                     Planned
                   </a>
@@ -83,6 +100,7 @@ export default function Example() {
                         active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
                         'block w-full px-4 py-2 text-left text-sm'
                       )}
+                      onClick={() => handleSelect("Not Selected")}
                     >
                       Not Selected
                     </button>
@@ -94,5 +112,5 @@ export default function Example() {
         )}
       </Transition>
     </Menu>
-  )
+  );
 }
